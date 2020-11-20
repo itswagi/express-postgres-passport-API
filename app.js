518eeec54ca14020b1a5dc8f99a1c61b60448d4c
itswagi <43064115+itswagi@users.noreply.github.com>
2020-11-20 17:47:22 +0500

    Added Sequelize with postgres

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-
+const sequelize = require('./src/db')
 const app = express()
 
 app.use(bodyParser.json())
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+    try{
+        await sequelize.sync(
+            //{force: true}
+        )
+        console.log('Connect to database')
+    }catch(error){
+        console.error(`Error: Cannot connect to database ${error}`)
+    }
 })
--- src/db/db.js
+++ src/db/db.js
@@ -1,1 +1,14 @@
-const { Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    logging: false
+
+})
+
+module.exports = sequelize;
